4ebb2bc5f7ae089b3e0ee6a7b670c2068786e27b
Tong Shen <TongShen2000@gmail.com>
2022-10-20 21:41:26 +1100

    update model in s3prl example

--- s3prl/upstream/example/expert.py
+++ s3prl/upstream/example/expert.py
@@ -35,7 +35,7 @@ class UpstreamExpert(nn.Module):
         Main.using("Flux")
         Main.using("BSON: @load")
         Main.using("Random")
-        Main.eval('@load "/srv/scratch/z5195063/360hModel.bson" trained_model post_net')
+        Main.eval('@load "/srv/scratch/z5195063/360hModel_v3.bson" trained_model post_net')
 
         print(
             f"{self.name} - You can use model_config to construct your customized model: {model_config}"
