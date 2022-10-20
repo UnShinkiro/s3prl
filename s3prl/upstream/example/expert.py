e9958c547953208c62f5e1846a3f5213baa90c10
Tong Shen <TongShen2000@gmail.com>
2022-10-21 02:54:33 +1100

    .

--- s3prl/upstream/example/expert.py
+++ s3prl/upstream/example/expert.py
@@ -75,7 +75,8 @@ class UpstreamExpert(nn.Module):
             Main.data = features[file_idx,:,:]
             Main.eval('data = Float32.(data)')
             Main.eval('data = [data[frame_idx,:] for frame_idx=1:size(data)[1]]')
-            feature = Main.eval('feature = trained_model.(data |> gpu)')
+            Main.eval('input = data[1:end] |> gpu')
+            feature = Main.eval('feature = trained_model.(input)')
             ret_feature.append(feature)
         
         ret_feature = np.asarray(ret_feature)
