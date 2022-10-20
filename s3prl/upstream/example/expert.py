9a29a1eedc13cf5cf98434b64b09d803e6604e27
Tong Shen <TongShen2000@gmail.com>
2022-10-21 01:47:09 +1100

    trying with gpu

--- s3prl/upstream/example/expert.py
+++ s3prl/upstream/example/expert.py
@@ -38,7 +38,7 @@ class UpstreamExpert(nn.Module):
         Main.using("CUDA")
         Main.using("Random")
         Main.eval('@load "/srv/scratch/z5195063/360hModel_v3.bson" trained_model post_net')
-        Main.eval('trained_model')
+        Main.eval('trained_model |> gpu')
 
         print(
             f"{self.name} - You can use model_config to construct your customized model: {model_config}"
@@ -79,8 +79,8 @@ class UpstreamExpert(nn.Module):
             Main.data = features[file_idx,:,:]
             Main.eval('data = Float32.(data)')
             Main.eval('data = [data[frame_idx,:] for frame_idx=1:size(data)[1]]')
-            Main.eval('print(size(data))')
-            feature = Main.eval('feature = trained_model.(data)')
+            Main.eval('input = data |> gpu')
+            feature = Main.eval('feature = trained_model.(input) |> gpu')
             ret_feature.append(feature)
         
         ret_feature = np.asarray(ret_feature)
