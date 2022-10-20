4fe156f261779a3d0e833a36fe4d95d948bd06f2
Tong Shen <TongShen2000@gmail.com>
2022-10-20 22:44:02 +1100

    fix gpu

--- s3prl/upstream/example/expert.py
+++ s3prl/upstream/example/expert.py
@@ -77,7 +77,8 @@ class UpstreamExpert(nn.Module):
             Main.eval('Flux.reset!(trained_model)')
             Main.data = features[file_idx,:,:]
             Main.eval('data = Float32.(data)')
-            Main.eval('data = [data[:,frame_idx] for frame_idx=1:size(data)[2]] |> gpu')
+            Main.eval('data = [data[:,frame_idx] for frame_idx=1:size(data)[2]]')
+            Main.eval('data |> gpu')
             feature = Main.eval('feature = trained_model.(data)')
             ret_feature.append(feature)
         
