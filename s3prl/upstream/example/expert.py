0200d99fab82c3dab210915b4a14e2ba152d6a48
Tong Shen <TongShen2000@gmail.com>
2022-10-26 16:41:29 +1100

    ..

--- s3prl/upstream/example/expert.py
+++ s3prl/upstream/example/expert.py
@@ -82,7 +82,7 @@ class UpstreamExpert(nn.Module):
             Main.data = file
             Main.eval('data = Float32.(data)')
             #Main.eval(f'data = [data[frame_idx,:] for frame_idx=1:{feat_lengths[count].item()}]')
-            Main.eval(f'data = [cu(data[frame_idx,:]) for frame_idx=1:{feat_lengths[count].item()}]')
+            Main.eval(f'data = [data[frame_idx,:] for frame_idx=1:{feat_lengths[count].item()}]')
             #Main.eval('CUDA.allowscalar(true)')
             #Main.eval('data = data |> gpu')
             feature = Main.eval(f'feature = [idx <= size(data)[1] ? trained_model(data[idx]) : zeros(Float32,512) for idx=1:{length}]')
