73d28c85013d2a8655373fb9b2306f47cd3c0742
Tong Shen <TongShen2000@gmail.com>
2022-10-26 14:13:25 +1100

    ,

--- s3prl/upstream/example/expert.py
+++ s3prl/upstream/example/expert.py
@@ -80,9 +80,10 @@ class UpstreamExpert(nn.Module):
             Main.data = file
             Main.eval('data = Float32.(data)')
             Main.eval('data = [data[frame_idx,:] for frame_idx=1:size(data)[1]]')
-            feature = Main.eval('feature = [trained_model(gpu(frame)) for frame in data]')
-            print(feature)
-            ret_feature.append(feature)
+            Main.eval('feature = [trained_model(gpu(frame)) for frame in data]')
+            Main.eval('print(size(feature))')
+            #print(feature)
+            #ret_feature.append(feature)
         
         ret_feature = np.asarray(ret_feature)
         ret_feature = torch.from_numpy(ret_feature).cuda()
